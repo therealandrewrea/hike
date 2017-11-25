d121424b3c2674de858a56900ed04c06def068b4
Andy Rea <therealandrewrea@gmail.com>
2017-11-25 11:39:18 -0600

    pushing source folder

---
+++ src/main/java/org/launchcode/Hike/Controllers/HomeController.java
@@ -0,0 +1,37 @@
+package org.launchcode.Hike.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("home")
+public class HomeController {
+
+    static ArrayList<String> hikes = new ArrayList<>();
+
+    @RequestMapping(value="")
+    public String index(Model model) {
+
+        model.addAttribute("hikes", hikes);
+        model.addAttribute("title", "Take A Hike!");
+        return "home/index";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String DisplayAdd(Model model) {
+
+        model.addAttribute("title", "Share A Hike");
+        return "home/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String ProcessAdd(@RequestParam String hikeName) {
+        hikes.add(hikeName);
+        return "redirect:";
+    }
+}
---
+++ src/main/java/org/launchcode/Hike/HikeApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.Hike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikeApplication.class, args);
+	}
+}
