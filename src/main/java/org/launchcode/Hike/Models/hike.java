1ced6d8aed9650306d3d24e1c0615a739c52f6db
Andy Rea <therealandrewrea@gmail.com>
2017-11-29 10:29:45 -0600

    added hikeID to hike object

--- src/main/java/org/launchcode/Hike/Models/hike.java
+++ src/main/java/org/launchcode/Hike/Models/hike.java
@@ -5,6 +5,7 @@ public class hike {
     private String name;
     private String description;
     private String location;
+    private int hikeId;
 //  just including text added fields for now, location may ultimately be coordinates //
 //  tags may be included in this class/object as well, need to see what works best //
 
