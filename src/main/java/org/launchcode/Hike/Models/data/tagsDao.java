8beb62836a9cdfe39f82de0f6b69f408f2a3b82b
Andy Rea <therealandrewrea@gmail.com>
2017-11-30 16:09:45 -0600

    added tagsDao

---
+++ src/main/java/org/launchcode/Hike/Models/data/tagsDao.java
@@ -0,0 +1,12 @@
+package org.launchcode.Hike.Models.data;
+
+import org.launchcode.Hike.Models.tags;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface tagsDao extends CrudRepository<tags, Integer> {
+}
