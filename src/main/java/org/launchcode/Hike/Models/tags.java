f6d95784784be3f78d1fb8f814b27498e458ff6c
Andy Rea <therealandrewrea@gmail.com>
2017-11-30 15:49:44 -0600

    added tags object for tracking labels and categories assigned to hikes

--- src/main/java/org/launchcode/Hike/Models/tags.java
+++ src/main/java/org/launchcode/Hike/Models/tags.java
@@ -1,6 +1,33 @@
 package org.launchcode.Hike.Models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class tags {
 
+    @NotNull
+    @Size(min=3, max=20, message = "Tags must be longer than 3 characters and shorter than 20")
+    private String name;
 
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public tags () {} //default constructor//
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
